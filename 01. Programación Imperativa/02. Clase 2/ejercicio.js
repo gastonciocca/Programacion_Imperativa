ada91bc14646641fc7283e92730d782a65b0071f
gastonciocca <100099357+gastonciocca@users.noreply.github.com>
2022-05-23 22:58:10 -0300

    Add files via upload
    Agrega toda la carpeta

---
+++ 01. Programación Imperativa/02. Clase 2/ejercicio.js
@@ -0,0 +1,31 @@
+// Definir variables para una actividad
+// ACTIVIDAD -> Pelar fruta
+
+let cantidad = 3;
+console.log(cantidad);
+
+const tipoFruta = "Naranja";
+console.log(tipoFruta);
+
+let necesitaCuchillo = true;
+console.log(necesitaCuchillo);
+
+let horaComida = 14;
+console.log(horaComida);
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+let otraFruta  = prompt ("Qué otra fruta quiere comer?");
+console.log(`Aquí dejo su ${otraFruta}!!!`);
+
+
+// Actividad Extra
+
+let remeras = "Cajon 1";
+let pantalones = "Cajon 2";
+
+remeras = "Cajon 2";
+pantalones = "Cajon 1";
+
+console.log(remeras, pantalones);
+
